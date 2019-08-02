71ca940ad2b76d8a31140941c1ec270572964b6f
Victorm0612 <rojasvictorvargas@gmail.com>
2019-08-01 23:44:47 -0500

    lectura archivos

---
+++ abrir.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main(int argc, char** argv) {
+	FILE* archivo = fopen("archivoPrueba", "r+");
+	int pid = fork();
+
+	if (pid == 0) {
+		fprintf(archivo, "Proceso hijo escribiendo \n");
+	}
+	else {
+		wait(NULL);
+		fprintf(archivo, "Proceso padre escribiendo \n");
+	}
+
+	int caracter; 
+	while ((caracter = fgetc(archivo)) != EOF) {
+		printf("%c\n", caracter);
+	}
+	fclose(archivo);
+
+	return 0;
+}
